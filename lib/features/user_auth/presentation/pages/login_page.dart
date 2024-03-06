c5659eeb7616a490aa2c78be92e23a8813cf0beb
unknown <emrekaradereli0@gmail.com>
2024-03-06 04:11:52 +0300

    google auth

--- lib/features/user_auth/presentation/pages/login_page.dart
+++ lib/features/user_auth/presentation/pages/login_page.dart
@@ -1,13 +1,14 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_final_fields
+// ignore_for_file: prefer_final_fields, no_leading_underscores_for_local_identifiers, use_build_context_synchronously, prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterfirebase/features/user_auth/firebase_auth_implementation/firebase_auth_services.dart';
-import 'package:flutterfirebase/features/user_auth/presentation/pages/home_page.dart';
 import 'package:flutterfirebase/features/user_auth/presentation/pages/sign_up_page.dart';
 import 'package:flutterfirebase/features/user_auth/presentation/widgets/form_container_widget.dart';
 import 'package:flutterfirebase/global/common/toast.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../../firebase_auth_implementation/firebase_auth_services.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -17,176 +18,196 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-
-    final FirebaseAuthService _auth = FirebaseAuthService();
-    bool _isSigning = false;
-
+  bool _isSigning = false;
+  final FirebaseAuthService _auth = FirebaseAuthService();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
   @override
   void dispose() {
-
     _emailController.dispose();
     _passwordController.dispose();
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Login",
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Color.fromARGB(
-            255, 81, 38, 155), // Set the desired background color
+        automaticallyImplyLeading: false,
+        title: Text("Login"),
       ),
       body: Center(
         child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Login",
-                    style:
-                        TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                SizedBox(
-                  height: 30,
-                ),
-                FormContainerWidget(
-                  controller: _emailController,
-                  hintText: "Email",
-                  isPasswordField: false,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                FormContainerWidget(
-                  controller: _passwordController,
-                  hintText: "Password",
-                  isPasswordField: true,
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                GestureDetector(
-                  onTap: _signIn,
-                  child: Container(
-                    width: double.infinity,
-                    height: 45,
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 111, 13, 240),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: _isSigning ? CircularProgressIndicator(color: Colors.white):  Text(
-                        "Login",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Login",
+                style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              FormContainerWidget(
+                controller: _emailController,
+                hintText: "Email",
+                isPasswordField: false,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              FormContainerWidget(
+                controller: _passwordController,
+                hintText: "Password",
+                isPasswordField: true,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              GestureDetector(
+                onTap: () {
+                  _signIn();
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                ),
-                  SizedBox(
-                  height: 15,
-                ),
-                 GestureDetector(
-                 
-                  child: Container(
-                    width: double.infinity,
-                    height: 45,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all()
-                      
-                    ),
-                    
-                    child: Center(
-                      child:Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(FontAwesomeIcons.google),
-                          SizedBox(width: 5,),
-                          Text(
-                            "Sign in with google",
+                  child: Center(
+                    child: _isSigning
+                        ? CircularProgressIndicator(
+                            color: Colors.white,
+                          )
+                        : Text(
+                            "Login",
                             style: TextStyle(
-                              color: Colors.black,
+                              color: Colors.white,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ],
-                      ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              GestureDetector(
+                onTap: () {
+                  _signInWithGoogle();
+                },
+                child: Container(
+                  width: double.infinity,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.google,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "Sign in with Google",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Don't have an account yet ?"),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    GestureDetector(
-                       onTap: () {
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account?"),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  GestureDetector(
+                    onTap: () {
                       Navigator.pushAndRemoveUntil(
                         context,
                         MaterialPageRoute(builder: (context) => SignUpPage()),
-                            (route) => false,
+                        (route) => false,
                       );
                     },
-                      child: Text(
-                        "Sign Up",
-                        style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.bold,
-                        ),
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
                       ),
-                    )
-                  ],
-                )
-              ],
-            )),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 
   void _signIn() async {
-
-    setState((){
-
+    setState(() {
       _isSigning = true;
-
     });
- 
+
     String email = _emailController.text;
     String password = _passwordController.text;
-    // String username = _usernameController.text;
-  
+
     User? user = await _auth.signInWithEmailAndPassword(email, password);
 
-
-   setState((){
-
+    setState(() {
       _isSigning = false;
-
     });
 
     if (user != null) {
-      showToast(message: "Welcome ");
-      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+      showToast(message: "User is successfully signed in");
+      Navigator.pushNamed(context, "/home");
     } else {
-      showToast(message: "Some error occured");
+      showToast(message: "some error occured");
+    }
+  }
+
+  _signInWithGoogle() async {
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await _googleSignIn.signIn();
+
+      if (googleSignInAccount != null) {
+        final GoogleSignInAuthentication googleSignInAuthentication =
+            await googleSignInAccount.authentication;
+
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken,
+        );
+
+        await _firebaseAuth.signInWithCredential(credential);
+        Navigator.pushNamed(context, "/home");
+      }
+    } catch (e) {
+      showToast(message: "some error occured $e");
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,23 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfirebase/features/user_auth/presentation/pages/home_page.dart';
 import 'package:flutterfirebase/features/user_auth/presentation/pages/login_page.dart';
+import 'package:flutterfirebase/features/user_auth/presentation/pages/sign_up_page.dart';
 import 'features/app/splash_screen/splash_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  Platform.isAndroid 
-  ? await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: "AIzaSyD51Cy9MYBbYqyPf6CxfdQbMztWxqsEX4s",
-      appId: "1:629360165632:android:402a74ecb786b21d66a9e5",
-      messagingSenderId: "629360165632",
-      projectId: "fluttter-auth-83789",
-    ))
-  : await Firebase.initializeApp();
+
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: FirebaseOptions(
+          apiKey: "AIzaSyD51Cy9MYBbYqyPf6CxfdQbMztWxqsEX4s",
+          appId: "1:629360165632:android:402a74ecb786b21d66a9e5",
+          messagingSenderId: "629360165632",
+          projectId: "fluttter-auth-83789",
+        ))
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -31,9 +33,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Firebase',
-     home: SplashScreen(
-      child: LoginPage(),
-      )
+      routes: {
+        '/': (context) => SplashScreen(
+              // Here, you can decide whether to show the LoginPage or HomePage based on user authentication
+              child: LoginPage(),
+            ),
+        '/login': (context) => LoginPage(),
+        '/signUp': (context) => SignUpPage(),
+        '/home': (context) => HomePage(),
+      },
     );
   }
 }
